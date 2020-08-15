8cc023be731ee3791df7efecae7ce2502dfd4080
Lakshmi Veera <lakshmi87.veera@gmail.com>
2020-08-14 20:33:36 -0700

    top sub domain visits

---
+++ Strings/Solution.java
@@ -0,0 +1,28 @@
+package Strings;
+import java.util.*;
+
+class Solution {
+    public List<String> subDomainVisits(String[] cpdomains) {
+        Map<String, Integer> counts = new HashMap();
+        for (String domain: cpdomains) {
+            String[] cpinfo = domain.split("\\s+");
+            String[] frags = cpinfo[1].split("\\.");
+            int count = Integer.valueOf(cpinfo[0]);
+            String cur = "";
+            for (int i = frags.length - 1; i >= 0; --i) {
+                cur = frags[i] + (i < frags.length - 1 ? "." : "") + cur;
+                counts.put(cur, counts.getOrDefault(cur, 0) + count);
+            }
+        }
+
+        List<String> ans = new ArrayList();
+        for (String dom: counts.keySet())
+            ans.add("" + counts.get(dom) + " " + dom);
+        return ans;
+    }
+
+    public static void main(String... args) {
+        Solution solution = new Solution();
+        System.out.println(solution.subDomainVisits(new String[]{"9001 dev.lakshmi.com"}));
+    }
+}
