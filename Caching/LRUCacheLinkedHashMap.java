3779785c4dbb804ae2554965df2990f54862766e
Lakshmi Veera <lakshmi87.veera@gmail.com>
2020-08-14 21:35:48 -0700

    design LRU cache

---
+++ Caching/LRUCacheLinkedHashMap.java
@@ -0,0 +1,26 @@
+package Caching;
+import java.util.*;
+/**
+ * Design a data structure for LRU Cache
+ */
+class LRULinkedHashMap extends LinkedHashMap<Integer, Integer>{
+    private int capacity;
+
+    public LRULinkedHashMap(int capacity) {
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+}
