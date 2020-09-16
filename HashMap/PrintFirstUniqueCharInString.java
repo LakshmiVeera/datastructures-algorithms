92c891ab0bc5b9b4b22cb217ed07eff4530bf26d
Lakshmi Veera <lakshmi87.veera@gmail.com>
2020-09-16 10:17:35 -0700

    simplified logic

--- HashMap/PrintFirstUniqueCharInString.java
+++ HashMap/PrintFirstUniqueCharInString.java
@@ -1,23 +1,16 @@
-package Strings;
+package com.lakshmi.java;
 import java.util.*;
 
 class PrintFirstUniqueCharInString {
     public static int firstUniqChar(String s) {
-
-        char[] stringCharArray = s.toCharArray();
         LinkedHashMap<Character, Integer> map = new LinkedHashMap<Character, Integer>();
-
-        for (int i = 0; i < stringCharArray.length; i++) {
-            map.put(stringCharArray[i], map.getOrDefault(stringCharArray[i], 0) + 1);
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
         }
-        Iterator<Map.Entry<Character, Integer>> itr = map.entrySet().iterator();
-        int i = 0;
-        while (itr.hasNext()) {
-            Map.Entry<Character, Integer> entry = itr.next();
-            if (entry.getValue() == 1) {
-                return   s.indexOf(entry.getKey());
+        for( char c : map.keySet()) {
+            if (map.get(c) == 1) {
+                return s.indexOf(c);
             }
-            i++;
         }
         return -1;
     }
