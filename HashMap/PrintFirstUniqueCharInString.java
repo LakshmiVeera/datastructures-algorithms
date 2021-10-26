0ffdebe20799cb4aff49b41b0ae29d1e23ef9313
Lakshmi Veera <lakshmi87.veera@gmail.com>
2021-10-26 09:45:46 -0700

    making it more readable

--- HashMap/PrintFirstUniqueCharInString.java
+++ HashMap/PrintFirstUniqueCharInString.java
@@ -1,21 +1,21 @@
 package HashMap;
 
 import java.util.*;
-
+// Return the index of first unique char in the String or return -1 if doesn't exists.
 class PrintFirstUniqueCharInString {
-    public static int firstUniqChar(String s) {
-        LinkedHashMap<Character, Integer> map = new LinkedHashMap<Character, Integer>();
-        for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+    public static int firstUniqChar(String str) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int i = 0; i < str.length(); i++) {
+            map.put(str.charAt(i), map.getOrDefault(str.charAt(i), 0) + 1);
         }
         for( char c : map.keySet()) {
             if (map.get(c) == 1) {
-                return s.indexOf(c);
+                return str.indexOf(c);
             }
         }
         return -1;
     }
     public static void main(String... args) {
-        System.out.println(firstUniqChar("dddccdbba"));
+        System.out.println(firstUniqChar("lakshmilaksh"));
     }
 }
